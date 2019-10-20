b40232f9044cd0d988fe6451d392241dd38db5e2
Ragavendhar T <ragavendhar.t@zohocorp.com>
2019-10-20 22:26:01 +0530

    next config file pushed

---
+++ next.config.js
@@ -0,0 +1,40 @@
+/* eslint-disable */
+const withCss = require('@zeit/next-css')
+
+module.exports = withCss({
+  webpack: (config, { dev, isServer }) => {
+    if (isServer) {
+      const antStyles = /antd\/.*?\/style\/css.*?/
+      const origExternals = [...config.externals]
+      config.externals = [
+        (context, request, callback) => {
+          if (request.match(antStyles)) return callback()
+          if (typeof origExternals[0] === 'function') {
+            origExternals[0](context, request, callback)
+          } else {
+            callback()
+          }
+        },
+        ...(typeof origExternals[0] === 'function' ? [] : origExternals),
+      ]
+
+      config.module.rules.unshift({
+        test: antStyles,
+        use: 'null-loader',
+      });
+      const extractCSSPlugin = new ExtractTextPlugin({
+        filename: 'style.css',
+        disable: dev
+      })
+      config.module.rules.unshift({
+        test: /\.css$/,
+        use: cssLoaderConfig(extractCSSPlugin, {
+          cssModules,
+          dev,
+          isServer
+        })
+      })
+    }
+    return config
+  },
+})
